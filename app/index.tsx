c48af4e79c98293260ef0b9fac02aa97800d0891
Mariano Baliño <marianobalinho@gmail.com>
2024-08-09 02:18:55 -0300

    first commit, add nativewind setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import '../constants/main.css'
+
+export default function HomeScren() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl">Hello world</Text>
+    </View>
+  );
+}
